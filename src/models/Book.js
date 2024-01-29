805d92d5ed04d9a8af175842276ced32e34318d6
Luan Bagio Compagnoni <luancmpg@gmail.com>
2024-01-28 21:47:01 -0300

    Added more validations in Book Model

--- src/models/Book.js
+++ src/models/Book.js
@@ -5,9 +5,9 @@ const bookSchema = new mongoose.Schema({
   id: {type: mongoose.Schema.Types.ObjectId },
   title: { type: String, required: [true, 'Book title is required']},
   price: { type: Number },
-  pages: { type: Number },
+  pages: { type: Number, min: [10, 'The number of pages must be between 10 and 5000'], max: [5000, 'The number of pages must be between 10 and 5000']  },
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'authors', required: [true, 'Author is required']},
-  editor: { type: String, required: [true, 'Editor is required'] }
+  editor: { type: String, required: [true, 'Editor is required'], enum: {values: ['Super Editor', 'Editor One'], message: 'The editor {VALUE} is not a valid value'} }
 }, { versionKey: false });
 
 const book = mongoose.model('book', bookSchema);
